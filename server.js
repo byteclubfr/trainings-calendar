6f7bf47c140cbd735d7739e6d79a991acaae3128
Nicolas Chambrier <naholyr@gmail.com>
2014-11-14 16:34:34 +0100

    npm start = live reload or prod-ready server

--- server.js
+++ server.js
@@ -1,3 +1,35 @@
-console.error("Not implemented yet");
+"use strict";
 
-process.exit(1);
+/* eslint-env node */
+/* globals open:true */ // allows overriding open
+
+var connect = require("connect");
+var optimist = require("optimist");
+var http = require("http");
+var path = require("path");
+var open = require("open");
+var serveStatic = require("instant");
+var serveFavicon = require("serve-favicon");
+var logger = require("morgan");
+var compression = require("compression");
+var errorHandler = require("errorhandler");
+
+
+var argv = optimist(process.argv);
+var root = path.resolve("src");
+
+var app = connect();
+
+app.use(serveStatic(root));
+app.use(serveFavicon(path.join(root, "images", "favicon.png")));
+app.use(logger("combined"));
+app.use(compression());
+app.use(errorHandler());
+
+http.createServer(app).listen(argv.port || 8080).on("listening", function () {
+  var url = "http://localhost:" + this.address().port;
+  console.log("Server ready: %s", url);
+  if (argv.open) {
+    open(url);
+  }
+});
